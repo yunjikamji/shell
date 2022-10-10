79b7795f420c90e1dcc20522f8943473dd4c1a31
yunjikamji <115465325+yunjikamji@users.noreply.github.com>
2022-10-10 17:52:38 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -19,7 +19,7 @@ read -p "choose ngrok region: " CRP
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull rattydave/docker-ubuntu-xrdp-mate-custom:20.04
+docker pull dorowu/ubuntu-desktop-lxde-vnc:focal
 clear
 echo "===================================="
 echo "Start RDP"
@@ -36,4 +36,4 @@ echo "Keep support akuh.net thank you"
 echo "Wait 1 minute to finish bot"
 echo "===================================="
 echo "===================================="
-docker run --rm -p 3388:3389 rattydave/docker-ubuntu-xrdp-mate-custom:20.04 > /dev/null 2>&1
+docker run --rm -p 3388:3389 dorowu/ubuntu-desktop-lxde-vnc:focal > /dev/null 2>&1
